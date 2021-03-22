2e0b055f4e42e75314a70c8e2872be394427f4fe
Michael Rodriguez <mikeyrod22@gmail.com>
2021-03-21 22:23:23 -0500

    color code event section based on current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 let currentDayEl = document.getElementById("currentDay");
 currentDayEl.innerText = moment().format("[Today is] dddd, MMMM Do YYYY");
+
+let timeblockEls = document.getElementsByClassName("time-block");
+function colorCode() {
+    for (var i = 0; i < timeblockEls.length; i++) {
+        let hour = parseInt(timeblockEls[i].children[0].innerText);
+        if (hour < 9) {
+            hour = hour + 12;
+        }
+        if (moment().hour() < hour) {
+            timeblockEls[i].children[1].className += " future";
+        } else if (moment().hour() > hour) {
+            timeblockEls[i].children[1].className += " past";
+        } else {
+            timeblockEls[i].children[1].className += " present";
+        }
+    }
+}
+colorCode();
